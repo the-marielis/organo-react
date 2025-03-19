62ac9f7e9cc28f2083a735957b5359e8a505d3c7
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-03-19 19:52:55 -0300

    initial commits

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png"
+            alt="Banner da Organização"/>
+        </header>
+    );
+}
+
+export default Banner;
