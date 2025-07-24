5359ea6535f28ffee1e1d4bc2275b94086d3e198
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-07-23 22:08:16 -0300

    arquivos iniciais do componente Time, responsável por renderizar os dados de colaboradores cadastrados em cada categoria

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from './componentes/Banner';
 import { useState } from 'react';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -15,6 +16,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
+      <Time nome="Devops" />
+      <Time nome="UX e Design" />
+      <Time nome="Mobile" />
+      <Time nome="Inovação e Gestão" />
     </div>
   );
 }
