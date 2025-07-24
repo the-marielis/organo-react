b926fefc09400b2d9fa03888a879578d6b64e6c0
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-07-24 14:17:09 -0300

    ajuste no componente time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
+  const times = [
+    { nome: 'Programação', corPrimaria: '#57C278', corSecundaria: '#D9F7E9' },
+    { nome: 'Front-End', corPrimaria: '#82CFFA', corSecundaria: '#E8F8FF' },
+    { nome: 'Data Science', corPrimaria: '#A6D157', corSecundaria: '#F0F8E2' },
+    { nome: 'Devops', corPrimaria: '#E06B69', corSecundaria: '#FDE7E8' },
+    { nome: 'UX e Design', corPrimaria: '#DB6EBF', corSecundaria: '#FAE9F5' },
+    { nome: 'Mobile', corPrimaria: '#FFBA05', corSecundaria: '#FFF5D9' },
+    { nome: 'Inovação e Gestão', corPrimaria: '#FF8A29', corSecundaria: '#FFEEDF' },
+  ]
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -16,13 +26,16 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      <Time nome="Programação" />
-      <Time nome="Frontend" />
-      <Time nome="Data Science" />
-      <Time nome="Devops" />
-      <Time nome="UX e Design" />
-      <Time nome="Mobile" />
-      <Time nome="Inovação e Gestão" />
+      
+      {times.map(time => (
+        <Time 
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />
+      ))}
     </div>
   );
 }
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,8 +2,8 @@ import './Time.css';
 
 const Time = (props) => {
     return (
-        <section className="time">
-            <h3>{props.nome}</h3>
+        <section className="time" style={{ backgroundColor: props.corSecundaria }}>  {/* As chaves duplas transformam o style em um objeto dentro do js */}
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
 
         </section>
     )
